d66de73305ec3ecc39ab3a08182cc2ce44dbb915
Matt Harbeson <matthew.harbeson@gmail.com>
2020-01-23 16:27:58 -0500

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,12 @@
+// Verify JS Running
+console.log("Up and running!");
+
+// Define 4 cards
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+// Log 2 cards
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
